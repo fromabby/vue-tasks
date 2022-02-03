8f07080b790fcaadcdb0cdb8832f92ab1152b70f
fromabby <60252891+fromabby@users.noreply.github.com>
2022-02-03 22:52:17 +0800

    Added delete task

---
+++ tasks-project/src/components/Button/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button 
+        :style="{background: color}" 
+        class="btn"
+        @click="onClick()"
+    >{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: {
+            type: String,
+            default: 'Button'
+        },
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log('clicked')
+        }
+    }
+}
+</script>
